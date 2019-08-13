6dfe71683cad1575cbdae2cdd56be36ad007e550
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-08-12 23:13:19 -0500

    diseño compuesto - statusBar  y títulos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:disenios_flutter/src/pages/basico_page.dart';
 import 'package:disenios_flutter/src/pages/scroll_page.dart';
@@ -11,6 +14,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // con PlatForm podemos indicar una condición para ejecutar codigo diferente en caso que se ocupe
+    // para diferenciar entre plataformas, en este caso android y ios
+    if (Platform.isAndroid) {
+      // cambiamos el statusBarColor (barra superior) a transparente
+      SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent
+      ));
+    } else if (Platform.isIOS) {
+      // cambiamos el statusBarColor (barra superior) a blanco
+      // solo que en los dispositivos ios lo que cambian de color son las letras
+      SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.white
+      ));
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -12,6 +12,13 @@ class BotonesPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           _fondoApp(),
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                _titulos(),
+              ],
+            ),
+          )
         ],
       )
     );
@@ -24,6 +31,7 @@ class BotonesPage extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       decoration: BoxDecoration(
+        // ingresamos el tipo de gradiente que queremos en este caso un linear
         gradient: LinearGradient(
           // donde empieza el gradiente
           begin: FractionalOffset(0.0, 0.6),
@@ -36,11 +44,12 @@ class BotonesPage extends StatelessWidget {
       ),
     );
 
+    // el transform.rotate gira un elemento
     final cajaRosada = Transform.rotate(
       angle: -pi /5.0,
       child: Container(
-        height: 360.0,
-        width: 360.0,
+        height: 330.0,
+        width: 330.0,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(80.0),
           gradient: LinearGradient(
@@ -56,6 +65,7 @@ class BotonesPage extends StatelessWidget {
     return Stack(
       children: <Widget>[
         gradiente,
+        // posisiona un elemento de manera que nosotros queramos
         Positioned(
           top: -100.0,
           child: cajaRosada,
@@ -63,6 +73,28 @@ class BotonesPage extends StatelessWidget {
       ],
     );
 
+  }
+
+  Widget _titulos() {
+
+
+    return SafeArea(
+
+      // se recomienda siempre englobar los elementos dentro de un container para poder agregar paddings y otras caracteristicas
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Classify transaction',
+            style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10.0,),
+            Text('Classify this transaction into particular category',
+            style: TextStyle(color: Colors.white, fontSize: 18.0))
+          ],
+        ),
+      ),
+    );
   }
 
 }
