6ac826a075c9bb8e4fb048add710736e5d690468
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-08-12 21:35:20 -0500

    diseño básico - parte 3

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -8,47 +8,66 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
           
           _crearImagen(),
           _crearTitulo(),
           _crearAcciones(),
           _texto(),
+          _texto(),
+          _texto(),
+          _texto(),
+          _texto(),
+          _texto(),
 
         ],
+        ),
       )
     );
   }
 
   Widget _crearImagen () {
-    return Image(
-            image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
-          );
+    return Container(
+      width: double.infinity,
+      child: Image(
+        height: 200.0,
+        fit: BoxFit.cover,
+              image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+            ),
+    );
   }
 
   Widget _crearTitulo () {
-    return Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: Row(
-              children: <Widget>[
-                // el widget Expanded expande a su hijo para ocupar todo el espacio posible sin afectar los demas elementos
-                Expanded(
-                  child: Column(
-                    // el crossAxisAligment ordena el contenido de manera vertical
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Lago con un puente', style: estiloTiltulo),
-                      SizedBox(height: 7.0),
-                      Text('Este lago se encuentra en Ameca XD', style: estiloSubTiltulo)
-                    ],
-                  ),
-                ),
-                Icon(Icons.star, color: Colors.red, size: 30.0),
-                Text('41', style: TextStyle(fontSize: 20.0),)
-              ],
+    /* 
+    el SafeArea sirve despliega información en el espacio que es visible en la pantalla
+    esto nos srive mucho para evitar el despliege de información en las barras que traen al gunos dispositivos
+    como el iPhone X
+    */ 
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            // el widget Expanded expande a su hijo para ocupar todo el espacio posible sin afectar los demas elementos
+            Expanded(
+              child: Column(
+                // el crossAxisAligment ordena el contenido de manera vertical
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Lago con un puente', style: estiloTiltulo),
+                  SizedBox(height: 7.0),
+                  Text('Este lago se encuentra en Ameca XD', style: estiloSubTiltulo)
+                ],
+              ),
             ),
-          );
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text('41', style: TextStyle(fontSize: 20.0),)
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _crearAcciones () {
@@ -64,12 +83,14 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _accion (IconData icon, String text) {
-    return Column(
-      children: <Widget>[
-        Icon(icon, color: Colors.blue, size: 40.0,),
-        SizedBox(height: 5.0,),
-        Text(text, style: TextStyle(fontSize: 15.0, color: Colors.blue),)
-      ],
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          Icon(icon, color: Colors.blue, size: 40.0,),
+          SizedBox(height: 5.0,),
+          Text(text, style: TextStyle(fontSize: 15.0, color: Colors.blue),)
+        ],
+      ),
     );
   }
 
